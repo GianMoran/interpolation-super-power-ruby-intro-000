8df3f576462d3900059636b9f9552c3ca1d7d98f
Gian Moran <gpmoran_@outlook.com>
2019-12-29 02:15:20 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -4,6 +4,6 @@ require 'pry'
 def display_rainbow(array) 
   letters = ["R", "O", "Y", "G", "B", "I", "V"]
   merge_array = letters[0] +":" + " "+ array[0]+  ", " + letters[1] +":"+ " "+ array[1] + ","+letters[2] +":" + " " + array[2]+  ", " +letters[3] +":" + " " + array[3]+  ", " + letters[4] +":" + " "+ array[4]+  ", " + letters[5] +":"+ " "+ array[5] + " "+letters[6] +":" + " "+ array[6]
-binding.pry
+
 merge_array
 end
