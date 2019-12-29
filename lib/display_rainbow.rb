a4e22331da12c8326219ef7014e5681729750bdb
Gian Moran <gpmoran_@outlook.com>
2019-12-29 02:10:09 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -3,6 +3,6 @@ require 'pry'
 
 def display_rainbow(array) 
   letters = ["R", "O", "Y", "G", "B", "I", "V"]
-new_array = (array + letters).join(" ")
-binding.pry
+merge_array = letters[0] +":" + array[0]+  ", " + letters[1] +":"+ array[1] + " "+letters[2] +":" + array[2]+  ", " +letters[3] +":" + array[3]+  ", " 
+
 end
